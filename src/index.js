55fe2e6c5dc260ba1b28ef9890fc26d55e1a95df
Kasheefah009 <kasheefahadedokun@gmail.com>
2026-05-05 20:01:31 +0100

    Updated files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import express from "express"
 import userRoutes from "./routes/user.routes.js"
 const app = express()
 
-app.use("/api, userRoutes")
+app.use("/api", userRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000")
